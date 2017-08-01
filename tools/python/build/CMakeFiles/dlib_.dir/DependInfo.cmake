2c51dca899cf12bc2daedfbcfd5f7fe11dad7c1d
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-08-02 01:20:23 +0530

    dlib

---
+++ tools/python/build/CMakeFiles/dlib_.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abhishek/dlib-19.4/tools/python/src/basic.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/basic.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/cca.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/cca.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/correlation_tracker.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/correlation_tracker.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/decision_functions.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/decision_functions.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/dlib.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/dlib.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/face_recognition.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/face_recognition.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/gui.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/gui.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/image.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/image.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/matrix.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/matrix.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/object_detection.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/object_detection.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/other.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/other.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/rectangles.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/rectangles.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/sequence_segmenter.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/sequence_segmenter.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/shape_predictor.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/shape_predictor.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/svm_c_trainer.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/svm_c_trainer.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/svm_rank_trainer.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/svm_rank_trainer.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/svm_struct.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/svm_struct.cpp.o"
+  "/home/abhishek/dlib-19.4/tools/python/src/vector.cpp" "/home/abhishek/dlib-19.4/tools/python/build/CMakeFiles/dlib_.dir/src/vector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DLIB_VERSION=19.4.0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/abhishek/anaconda3/envs/opencv/include"
+  "/home/abhishek/anaconda3/envs/opencv/include/python3.5m"
+  "/home/abhishek/dlib-19.4/dlib/.."
+  "/home/abhishek/anaconda3/envs/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/abhishek/dlib-19.4/tools/python/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
